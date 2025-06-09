81ee9d010eedae61cadddbc7d32558a891d54495
David Ferreira <65172017+tcFeyn@users.noreply.github.co>
2025-06-09 23:33:36 +0100

    Added mini.starter

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -14,6 +14,30 @@ return {
 
       -- Auto pairs
       require('mini.pairs').setup()
+
+      -- Stater dashboard
+      local starter = require('mini.starter')
+      starter.setup({
+        evaluate_single = true,
+        header = table.concat({
+          [[  /\ \▔\___  ___/\   /(●)_ __ ___  ]],
+          [[ /  \/ / _ \/ _ \ \ / / | '_ ` _ \ ]],
+          [[/ /\  /  __/ (_) \ V /| | | | | | |]],
+          [[\_\ \/ \___|\___/ \_/ |_|_| |_| |_|]],
+          [[───────────────────────────────────]],
+        }, "\n"),
+        footer = os.date(),
+        items = {
+          { name = " Create file", action = ":enew", section = "Menu" },
+          { name = " Open file", action = require('telescope.builtin').find_files, section = "Menu" },
+          { name = " Quit Neovim", action = ":qa!", section = "Menu" },
+          starter.sections.recent_files(10, false),
+        },
+        content_hooks = {
+          starter.gen_hook.adding_bullet(" "),
+          starter.gen_hook.aligning("center", "center"),
+        },
+      })
     end
   },
 }
