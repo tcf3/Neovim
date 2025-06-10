604a5935ad8b83da7d173db954b29a1b218ec4e5
David Ferreira <65172017+tcFeyn@users.noreply.github.co>
2025-06-10 20:24:55 +0100

    Added completion to render md

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -5,4 +5,9 @@ return
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
+  config = function()
+    require('render-markdown').setup({
+      completions = { blink = { enabled = true } },
+    })
+  end
 }
