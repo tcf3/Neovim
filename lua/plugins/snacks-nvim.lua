f82ad92ab5bfe27294b9c904ddb6a0215c24e327
David Ferreira <65172017+tcFeyn@users.noreply.github.com>
2025-10-01 21:51:28 +0100

    Removed snacks-term

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -7,15 +7,11 @@ return {
     explorer = {},
     picker = {},
     indent = {},
-    terminal = {
-      win = { style = "terminal" },
-    },
   },
 
   keys = {
     { "<leader>e", function() Snacks.explorer() end, desc = "Explorer Snacks (root dir)", },
     { "<leader>ff", function() Snacks.picker.files(opts) end, desc = "Snacks find files", },
     { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep picker", },
-    { "<leader>t", function() Snacks.terminal.toggle(cmd, opts) end, desc = "Terminal" }
   }
 }
