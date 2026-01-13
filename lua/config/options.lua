d0a121319641baee40175e176d9b9355c0542e9b
David Ferreira <dferreira@slvppod1.lab.dfcloud.cc>
2026-01-13 10:29:51 +0000

    Commented clipboard not needed?? -_-

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -88,15 +88,14 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Make nvim share tmux clipboard
-vim.g.clipboard = {
-  name = "osc52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
-
+--vim.g.clipboard = {
+--  name = "osc52",
+--  copy = {
+--    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+--    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+--  },
+--  paste = {
+--    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+--    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+--  },
+--}
