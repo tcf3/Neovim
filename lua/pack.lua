c81ed1fac642063680fe071de8922ddcd4db8edb
David Ferreira <65172017+tcFeyn@users.noreply.github.com>
2026-05-25 08:46:25 +0100

    Removed mason in favor of using brew

--- lua/pack.lua
+++ lua/pack.lua
@@ -4,5 +4,4 @@ vim.pack.add({
   "https://github.com/nvim-mini/mini.nvim",
   "https://github.com/folke/snacks.nvim",
   "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/mason-org/mason.nvim",
 })
